d1a00648e7f836c35bbf9cd3494ec20f568bf7d3
Araolaza Martin <67218632+Deddkal@users.noreply.github.com>
2024-03-19 14:06:55 -0300

    first commit

---
+++ IntroJS/main.js
@@ -0,0 +1,103 @@
+
+// console.log("Algo")
+
+// Tipo de datos
+// Datos primitivos
+// String, number, boolean, null, undefined
+
+// (mala palabra) var -- let -- const
+
+// const apellido = "Araolaza"
+
+// let nombre = "Martin"
+
+// // concatenar
+// console.log( "Hola, mi 'nombre' " + nombre + " y mi apellido es " + apellido )
+// console.log( 'hola como "estas"' )
+// console.log( ` ${5 + 5} ${apellido} ` ) 
+
+// let number1 = 100
+// let number2 = 20
+
+// console.log( number1 + number2)
+// console.log( number1 - number2)
+// console.log( number1 * number2)
+// console.log( number2 / number1)
+// console.log( number2 % number1)
+// console.log( number1 ** 0.5)
+
+/*  
+
+*/
+
+// let number 
+
+// console.log(number)
+
+// number = 9
+
+// console.log( number % 2 )
+
+// number++
+// number--
+// number += 10
+// number *= 2 // number = number * 2 
+
+// console.log(number * 2)
+
+// Operadores de comparacion
+// != !== == === < <= > >= 
+
+// console.log( "2" >= 0 )
+
+
+// Operadores logicos
+
+// AND &&   OR ||   NOT !
+
+// console.log( true && ( !true || false ) )
+// console.log( true || false )
+// console.log( false || true )
+// console.log( false || false)
+
+// console.log( !true )
+// console.log( !false )
+
+// console.log( 2 == true )
+// console.log( "" == false )
+
+// let nombre = "Martin"
+// let apellido = "Araolaza"
+
+
+// Objetos y arrays
+
+// let mentor = {
+//     // key || clave = value || valor
+//     nombre : "Martin",
+//     apellido : "Araolaza"
+// }
+
+// let mentor2 = {
+//     // key || clave = value || valor
+//     nombre : "Guille",
+//     apellido : "Cornetti"
+// }
+
+// let arrayMentores = [ mentor , mentor2 ]
+
+// console.log(mentor.nombre)
+
+// console.log(arrayMentores[1].nombre)
+
+
+// Interactuar con el usuario con popups
+
+// alert("Soy una ventana molesta")
+
+// let nombre = prompt("Ingrese su nombre")
+
+// let resultado = confirm("estas seguro?")
+
+// console.log(resultado)
+
